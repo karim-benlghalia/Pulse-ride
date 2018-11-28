749f7dbdc38eaf787e513551c2f4f859c5266315
etanboga <ege72282@gmail.com>
2018-11-28 15:00:50 -0800

    added login functionalit

--- FirebaseModels.js
+++ FirebaseModels.js
@@ -9,3 +9,33 @@ var config = {
 firebase.initializeApp(config)
 
 console.log('Javascript file is connected')
+
+function loginUser() {
+  let userEmail = document.getElementById('username_input').value
+  let userPassword = document.getElementById('password_input').value
+  firebase.auth().signInWithEmailAndPassword(userEmail, userPassword).then(
+    function() { // A hacky way to navigate to the next page
+      window.location.href = window.location.href.replace("index.html", "welcome-map.html")
+    }
+  )
+  .catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // ...
+    console.log(errorCode)
+    window.alert(errorMessage)
+  });
+}
+
+firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+    // User is signed in.
+    console.log(user)
+  } else {
+
+  }
+});
+
+
+
