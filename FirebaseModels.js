53a3696633c91507bc361a277794dd7a53c92a6f
git push -u origin <renee8688@gmail.com>
2018-11-30 01:02:52 -0800

    sign up/in

--- FirebaseModels.js
+++ FirebaseModels.js
@@ -1,14 +1,42 @@
+// var config = {
+//   apiKey: "AIzaSyBT10F4vX8JxcYKhdJfoi3JqhdWduWVNbk",
+//   authDomain: "pulse-ride.firebaseapp.com",
+//   databaseURL: "https://pulse-ride.firebaseio.com",
+//   projectId: "pulse-ride",
+//   storageBucket: "pulse-ride.appspot.com",
+//   messagingSenderId: "350869264943"
+// };
+// firebase.initializeApp(config)
 var config = {
-  apiKey: "AIzaSyBT10F4vX8JxcYKhdJfoi3JqhdWduWVNbk",
-  authDomain: "pulse-ride.firebaseapp.com",
-  databaseURL: "https://pulse-ride.firebaseio.com",
-  projectId: "pulse-ride",
-  storageBucket: "pulse-ride.appspot.com",
-  messagingSenderId: "350869264943"
+  apiKey: "AIzaSyDYk2XKh2RrlfyYSfBP7tdD0_wHoziCBFU",
+  authDomain: "pulse-ride188.firebaseapp.com",
+  databaseURL: "https://pulse-ride188.firebaseio.com",
+  projectId: "pulse-ride188",
+  storageBucket: "pulse-ride188.appspot.com",
+  messagingSenderId: "1040848890807"
 };
-firebase.initializeApp(config)
-
+firebase.initializeApp(config);
 console.log('Javascript file is connected')
+
+function registerUser(){
+  let email = document.getElementById('register_email').value;
+  let password = document.getElementById('register_password').value;
+  console.log(email, password);
+
+  firebase.auth().createUserWithEmailAndPassword(email, password)
+  .then(
+    function() { // A hacky way to navigate to the next page
+      window.location.href = window.location.href.replace("registration.html", "welcome-map.html")
+    }
+  )
+  .catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // ...
+  });
+}
+
 
 function loginUser() {
   let userEmail = document.getElementById('username_input').value
